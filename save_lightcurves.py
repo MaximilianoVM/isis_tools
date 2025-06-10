20a6d178424f0cc29110d1694670a700f5fccfdf
MaximilianoVM <maximiliano.vaca@uabc.edu.mx>
2025-06-09 18:00:47 -0700

    👷🏾 change set input from `int()` to `str()`

--- save_lightcurves.py
+++ save_lightcurves.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from czerny import czerny, czerny_plot
 
-set = int(input("ingresa el NUMERO del set. 🗃️ ej: 3 para images3, 2 para images2: \n"))
+set = str(input("ingresa el NUMERO del set. 🗃️ ej: 3 para images3, 2 para images2: \n"))
 color = str(input("ingresa el color para tus curvas \n ej. red 🔴, magenta 🟣, green 🟢, blue 🔵:\n"))
 
 
